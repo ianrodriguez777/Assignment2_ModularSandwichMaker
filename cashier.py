c22cf96a4edeba8da91c8ed4e7df8f181261e880
irodri10 <irodri10@charlotte.edu>
2024-07-28 13:45:18 -0400

    feat (process_coins): process_coins in cashier.py is completed
    Several prompts ask the user to insert change. Each type of coin is added to the total.

--- cashier.py
+++ cashier.py
@@ -5,7 +5,12 @@ class Cashier:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        print("Insert coins: ")
+        total = int(input("How many quarters?: ")) * 0.25
+        total += int(input("How many dimes?: ")) * 0.1
+        total += int(input("How many nickels?: ")) * 0.05
+        total += int(input("How many pennies?: ")) * 0.01
+        return total
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
